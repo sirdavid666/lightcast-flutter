4d13718989beba663f2a02ed73b8dea64fb6ad8b
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-03 00:39:43 +0100

    Make main function async and set device orientations
    Change main function to async and set preferred orientations.

--- apps/lightcast_director/lib/main.dart
+++ apps/lightcast_director/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app/director_app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   runApp(const DirectorApp());
 }
