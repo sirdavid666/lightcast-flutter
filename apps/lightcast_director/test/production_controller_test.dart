53b7947ddac39c6f3e49e9a4a1bc7bde4a0ae2d4
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-04 18:22:39 +0100

    Simplify director program workflow

--- apps/lightcast_director/test/production_controller_test.dart
+++ apps/lightcast_director/test/production_controller_test.dart
@@ -3,39 +3,24 @@ import 'package:lightcast_director/state/director_providers.dart';
 import 'package:lightcast_shared/lightcast_shared.dart';
 
 void main() {
-  test('TAKE publishes the complete Preview scene', () {
+  test('layout changes publish directly to Program', () {
     final controller = ProductionController();
     controller.setLayout(CameraLayout.pastorInCrowd);
 
-    final previewPastor = controller.state.previewScene.layers
-        .firstWhere((layer) => layer.id == 'pastor-video');
-    final programPastorBefore = controller.state.programScene.layers
+    final programPastor = controller.state.programScene.layers
         .firstWhere((layer) => layer.id == 'pastor-video');
 
-    expect(programPastorBefore.frame.x, 0);
-    expect(previewPastor.frame.x, .72);
-
-    controller.take();
-
-    final programPastorAfter = controller.state.programScene.layers
-        .firstWhere((layer) => layer.id == 'pastor-video');
-    expect(programPastorAfter.frame.x, .72);
+    expect(programPastor.frame.x, .72);
   });
 
-  test('ticker edits remain private until UPDATE TICKER', () {
+  test('ticker edits update Program immediately', () {
     final controller = ProductionController();
     controller.setTickerText('WELCOME TO LIGHTCAST');
 
-    final programTickerBefore = controller.state.programScene.layers
+    final programTicker = controller.state.programScene.layers
         .firstWhere((layer) => layer.id == 'ticker')
         .payload['text'];
-    expect(programTickerBefore, isNull);
 
-    controller.updateTicker();
-
-    final programTickerAfter = controller.state.programScene.layers
-        .firstWhere((layer) => layer.id == 'ticker')
-        .payload['text'];
-    expect(programTickerAfter, 'WELCOME TO LIGHTCAST');
+    expect(programTicker, 'WELCOME TO LIGHTCAST');
   });
 }
