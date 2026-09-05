6be4bf95c3f5b654c43f81dedae655fd535d8fcb
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-05 07:24:18 +0100

    Add import for dart:io in lan_camera_transport.dart

--- apps/lightcast_camera/lib/services/lan_camera_transport.dart
+++ apps/lightcast_camera/lib/services/lan_camera_transport.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
