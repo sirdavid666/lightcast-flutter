0d71d7320544afbdf51c295f1df154f1418d1ccc
sirdavid666 <olujinmiewaoluwa@gmail.com>
2026-09-05 11:52:23 +0100

    Add QR code and Riverpod imports to director_screen.dart

--- apps/lightcast_director/lib/screens/director_screen.dart
+++ apps/lightcast_director/lib/screens/director_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lightcast_shared/lightcast_shared.dart';
 import '../app/theme.dart';
